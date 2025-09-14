0f1ba4160a3fbc95eecb5e464a6ee8664746ae2c
djessy212 <djessyfarm@gmail.com>
2025-09-15 00:34:32 +0100

    Update Subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,1 +1,11 @@
-js/subjects/Subject.ts
+/// <reference path="Teacher.ts" />
+
+namespace Subjects {
+  export class Subject {
+    private teacher: Teacher;
+
+    setTeacher(teacher: Teacher): void {
+      this.teacher = teacher;
+    }
+  }
+}
