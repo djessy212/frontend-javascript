3773a6e948d9c4ba82abad6b0ea8c03edf1da69e
djessy212 <djessyfarm@gmail.com>
2025-09-15 00:38:44 +0100

    Update Cpp.ts

--- 0x04-TypeScript/task_4/js/subjects/Cpp.ts
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -1,1 +1,24 @@
-js/subjects/Cpp.ts
+/// <reference path="Teacher.ts" />
+
+namespace Subjects {
+  export class Subject {
+    protected teacher: Teacher;
+
+    setTeacher(teacher: Teacher): void {
+      this.teacher = teacher;
+    }
+  }
+
+  export class Cpp extends Subject {
+    getRequirements(): string {
+      return "Here is the list of requirements for Cpp";
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher && this.teacher.experienceTeachingC && this.teacher.experienceTeachingC > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return "No available teacher";
+    }
+  }
+}
