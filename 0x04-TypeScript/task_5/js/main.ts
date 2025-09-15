ef5fa52a39b4907545739829fa1b800178aa257b
djessy212 <djessyfarm@gmail.com>
2025-09-15 01:19:26 +0100

    Update main.ts

--- 0x04-TypeScript/task_5/js/main.ts
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -1,1 +1,34 @@
-task_5/js/main.ts
+export interface MajorCredits {
+  credits: number;
+  readonly brand: 'MajorCredits';
+}
+
+export interface MinorCredits {
+  credits: number;
+  readonly brand: 'MinorCredits';
+}
+// Define interfaces
+export interface MajorCredits {
+  credits: number;
+  readonly brand: 'MajorCredits';
+}
+
+export interface MinorCredits {
+  credits: number;
+  readonly brand: 'MinorCredits';
+}
+
+// Function to sum MajorCredits
+export function sumMajorCredits(
+  subject1: MajorCredits,
+  subject2: MajorCredits
+): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: 'MajorCredits',
+  };
+}
+
+// Function to sum MinorCredits
+export functio
+
